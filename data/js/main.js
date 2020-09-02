bf473d9305a1a61f0dc57150f53f8c7705735d7b
Mik_a <ilemik14@gmail.com>
2020-09-02 20:47:21 +0900

    package init

--- data/js/main.js
+++ data/js/main.js
@@ -1,12 +1,12 @@
-Object.keys(gameList).forEach((e) => {
-  let button = createButton(gameList[e], () => {window.location.href = "/lobby/" + e});
-  document.getElementById("game-list").appendChild(button);
-})
+// Object.keys(gameList).forEach((e) => {
+//   let button = createButton(gameList[e], () => {window.location.href = "/lobby/" + e});
+//   document.getElementById("game-list").appendChild(button);
+// })
 
-function createButton(name, callback){
-  let button = document.createElement("button");
-  button.innerHTML = name;
-  button.addEventListener('click' , callback);
-  return button;
-}
+// function createButton(name, callback){
+//   let button = document.createElement("button");
+//   button.innerHTML = name;
+//   button.addEventListener('click' , callback);
+//   return button;
+// }
 
