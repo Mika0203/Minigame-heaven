62e50abbbb5971d403841722622a77f3480bc4a1
Mik_a <ilemik@youcanstar.com>
2020-09-16 16:19:24 +0900

    add 2048 game

--- data/js/game_manager.js
+++ data/js/game_manager.js
@@ -1,9 +1,25 @@
 class GameManager {
     constructor(){
         this.score = 0;
+        this.isStart = false;
     }
     
-    init(){
+    init(){};
 
-    }
+    gameStart(){
+        console.debug("Game Start!!");
+        this.isStart = true;
+        this.onGameStart();
+    };
+
+    gameOver(){
+        let div = document.createElement("div");
+        div.innerHTML = "Game Over";
+        document.body.appendChild(div);
+        
+        console.debug("Game Over!!");
+        this.isStart = false;
+    };
+
+    onGameStart(){};
 }
